9b8abb491107b302110f454aca52afc8f625055c
Alexis Salecroix <alexis.salecroix@gmail.com>
2019-04-24 23:19:47 +0200

    Deuxieme commit,Section 5

---
+++ src/Controller/AdController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdController extends AbstractController
+{
+    /**
+     *
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+        $ads=$repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads,
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/ad/new", name="ads_create")
+     * @return Response
+     */
+    public function create(){
+        $ad=new Ad();
+        $form=$this->createForm(AdType::class, $ad);
+
+        return $this->render('ad/new.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public function show(Ad $ad){
+        return $this->render('ad/show.html.twig',[
+           'ad'=>$ad
+        ]);
+
+    }
+}
