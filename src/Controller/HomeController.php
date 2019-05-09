c96a00880c242bf94d230985cc0733c7acff7a00
Alexis Salecroix <alexis.salecroix@gmail.com>
2019-05-10 00:28:58 +0200

    Webpack encore

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,23 +13,13 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
      */
-    public function home(){
-
-        $prenoms = ["alexis" =>33,"alexandre" =>12,"anne" =>40];
-
-        return $this->render('home.html.twig',[
-            'title' => 'Bonjour les amis',
-            'age' => 31,
-            'prenoms'=>$prenoms
-        ]);
-    }
-
-    /**
-     * @Route("/hello/{prenom}", name="hello")
-     * @return Response
-     */
-    public function hello($prenom="anonyme"){
-        return new Response('Bonjour '.$prenom);
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
+        return $this->render(
+            'home.html.twig',
+            [
+                'ads'=> $adRepo->findBestAds(4),
+                'users' => $userRepo->findBestUsers(2),
+            ]
+        );
     }
 }
-?>
